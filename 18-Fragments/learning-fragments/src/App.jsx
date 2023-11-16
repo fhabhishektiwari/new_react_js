5bf7338bfec69a0c1fa47b2faf639f893038e287
fhabhishektiwari <abhishektiwarifh@gmail.com>
2023-11-16 21:46:22 +0530

    Added learning fragments

--- 18-Fragments/learning-fragments/src/App.jsx
+++ 18-Fragments/learning-fragments/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from "react";
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <React.Fragment>
+      {/* Either use <React.Fragment></React.Fragment> or use <></> */}
+      <h1>Healty Food Items</h1>
+      <ul className="list-group">
+        <li className="list-group-item">Dal</li>
+        <li className="list-group-item">Green Vegetables</li>
+        <li className="list-group-item">Curd</li>
+        <li className="list-group-item">Roti</li>
+        <li className="list-group-item">Rice</li>
+      </ul>
+    </React.Fragment>
+  );
+};
 
-export default App
+export default App;
