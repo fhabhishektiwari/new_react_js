69536e0286a42971b3e48072164a9a13d8d2a3bd
fhabhishektiwari <abhishektiwarifh@gmail.com>
2023-11-16 22:02:40 +0530

    added map methods

--- 18-Fragments/learning-fragments/src/App.jsx
+++ 18-Fragments/learning-fragments/src/App.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 const App = () => {
+  const listItems = ["Dal", "Green Vegetables", "Curd", "Roti", "Rice"];
   return (
     <React.Fragment>
       {/* Either use <React.Fragment></React.Fragment> or use <></> */}
       <h1>Healty Food Items</h1>
-      <ul className="list-group">
+      {/* <ul className="list-group">
         <li className="list-group-item">Dal</li>
         <li className="list-group-item">Green Vegetables</li>
         <li className="list-group-item">Curd</li>
         <li className="list-group-item">Roti</li>
         <li className="list-group-item">Rice</li>
+      </ul> */}
+      <ul className="list-group">
+        {listItems.map((item, index) => (
+          <li className="list-group-item" key={index}>
+            {item}
+          </li>
+        ))}
       </ul>
     </React.Fragment>
   );
