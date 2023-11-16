0feb7b3883cd105d33212db3b4793dd065de9181
fhabhishektiwari <abhishektiwarifh@gmail.com>
2023-11-16 23:10:49 +0530

    Added props and module css

--- 18-Fragments/learning-fragments/src/App.jsx
+++ 18-Fragments/learning-fragments/src/App.jsx
@@ -1,36 +1,16 @@
 import React from "react";
+import ErrorMessage from "./components/ErrorMessage";
+import FoodItems from "./components/FoodItems";
+import "./App.css";
 const App = () => {
-  // const listItems = ["Dal", "Green Vegetables", "Curd", "Roti", "Rice"];
-  const listItems = [];
-  // conditional rendering
-  // if-else
-  // if (listItems.length === 0) {
-  //   return <h1>I am still hungry..</h1>;
-  // }
-  // ternary
-  // let emptyMsg = listItems.length === 0 ? <h1>I am still hungry..</h1> : null;.
-  // logical operator (&&)
-  let emptyMsg = listItems.length === 0 && <h1>I am still hungry..</h1>;
+  const foodItems = ["Dal", "Green Vegetables", "Curd", "Roti", "Rice"];
+  // const foodItems = [];
 
   return (
     <React.Fragment>
-      {/* Either use <React.Fragment></React.Fragment> or use <></> */}
-      <h1>Healty Food Items</h1>
-      {/* <ul className="list-group">
-        <li className="list-group-item">Dal</li>
-        <li className="list-group-item">Green Vegetables</li>
-        <li className="list-group-item">Curd</li>
-        <li className="list-group-item">Roti</li>
-        <li className="list-group-item">Rice</li>
-      </ul> */}
-      {emptyMsg}
-      <ul className="list-group">
-        {listItems.map((item, index) => (
-          <li className="list-group-item" key={index}>
-            {item}
-          </li>
-        ))}
-      </ul>
+      <h1 className="display-6 kg-title">Healty Food Items</h1>
+      <ErrorMessage items={foodItems} />
+      <FoodItems items={foodItems} />
     </React.Fragment>
   );
 };
