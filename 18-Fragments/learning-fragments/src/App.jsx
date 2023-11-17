264d89eeb9499005e552bf434087c054a6240975
fhabhishektiwari <abhishektiwarifh@gmail.com>
2023-11-17 14:03:36 +0530

    Added children props

--- 18-Fragments/learning-fragments/src/App.jsx
+++ 18-Fragments/learning-fragments/src/App.jsx
@@ -1,17 +1,25 @@
-import React from "react";
 import ErrorMessage from "./components/ErrorMessage";
 import FoodItems from "./components/FoodItems";
-import "./App.css";
+import styles from "./App.module.css";
+import Container from "./components/Container";
 const App = () => {
   const foodItems = ["Dal", "Green Vegetables", "Curd", "Roti", "Rice"];
   // const foodItems = [];
 
   return (
-    <React.Fragment>
-      <h1 className="display-6 kg-title">Healty Food Items</h1>
-      <ErrorMessage items={foodItems} />
-      <FoodItems items={foodItems} />
-    </React.Fragment>
+    <>
+      <Container>
+        <h1 className={`display-6 ${styles.kgTitle}`}>Healty Food Items</h1>
+        <ErrorMessage items={foodItems} />
+        <FoodItems items={foodItems} />
+      </Container>
+      <Container>
+        <p>
+          Above is the list of healthy foods that are good for your health and
+          well being.
+        </p>
+      </Container>
+    </>
   );
 };
 
