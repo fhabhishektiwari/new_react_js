8adaecc258aa73a96fc036676de4b1ccb3c92be4
fhabhishektiwari <abhishektiwarifh@gmail.com>
2023-11-18 11:23:50 +0530

    Added form and using useRef hooks

--- projects/6-todo-app-version-three/src/components/AddTodo.jsx
+++ projects/6-todo-app-version-three/src/components/AddTodo.jsx
@@ -1,67 +1,77 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Button from "./Button";
 import PropTypes from "prop-types";
 import { IoMdAdd } from "react-icons/io";
 function AddTodo({ onNewItem }) {
-  const [todoName, setTodoName] = useState("");
-  const [dueDate, setDueDate] = useState("");
+  // const [todoName, setTodoName] = useState("");
+  // const [dueDate, setDueDate] = useState("");
 
-  const handleNameChange = (event) => {
-    setTodoName(event.target.value);
-  };
-  const handleDateChange = (event) => {
-    setDueDate(event.target.value);
-  };
+  const todoNameElement = useRef();
+  const dueDateElement = useRef();
 
-  const handleAddButtonClicked = () => {
+  // const handleNameChange = (event) => {
+  //   setTodoName(event.target.value);
+  // };
+  // const handleDateChange = (event) => {
+  //   setDueDate(event.target.value);
+  // };
+
+  const handleAddButtonClicked = (event) => {
+    event.preventDefault();
+    // console.log(event);
+    const todoName = todoNameElement.current.value;
+    const dueDate = dueDateElement.current.value;
+    todoNameElement.current.value = "";
+    dueDateElement.current.value = "";
     onNewItem(todoName, dueDate);
-    setTodoName("");
-    setDueDate("");
+    // setTodoName("");
+    // setDueDate("");
   };
 
   return (
     <div className="container">
       <div className="row mt-3">
-        <div className="col-6">
-          <div className="input-group mb-3">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter todo here"
-              aria-label="todo"
-              aria-describedby="basic-addon1"
-              value={todoName}
-              onChange={handleNameChange}
-            />
+        <form style={{ display: "block" }} onSubmit={handleAddButtonClicked}>
+          <div className="col-6">
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter todo here"
+                aria-label="todo"
+                aria-describedby="basic-addon1"
+                ref={todoNameElement}
+                // value={todoName}
+                // onChange={handleNameChange}
+              />
+            </div>
           </div>
-        </div>
-        <div className="col-4">
-          <div className="input-group mb-3">
-            <input
-              type="date"
-              className="form-control"
-              placeholder="Enter todo here"
-              aria-label="todo"
-              aria-describedby="basic-addon1"
-              value={dueDate}
-              onChange={handleDateChange}
-            />
+          <div className="col-4">
+            <div className="input-group mb-3">
+              <input
+                type="date"
+                className="form-control"
+                placeholder="Enter todo here"
+                aria-label="todo"
+                aria-describedby="basic-addon1"
+                ref={dueDateElement}
+                // value={dueDate}
+                // onChange={handleDateChange}
+              />
+            </div>
           </div>
-        </div>
-        <div className="col-2">
-          <Button
-            className={"btn btn-success custom-btn"}
-            onClick={handleAddButtonClicked}
-          >
-            <IoMdAdd />
-            <span style={{ marginLeft: "3px" }}>Add</span>
-          </Button>
-        </div>
+          <div className="col-2">
+            <Button className={"btn btn-success custom-btn"}>
+              <IoMdAdd />
+              <span style={{ marginLeft: "3px" }}>Add</span>
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   );
 }
-AddTodo.propTypes = {
-  onNewItem: PropTypes.func.isRequired,
-};
+// AddTodo.propTypes = {
+//   onNewItem: PropTypes.func.isRequired,
+// };
 export default AddTodo;
